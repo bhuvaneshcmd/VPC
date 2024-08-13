49bdc007b1ea657094ce7e090fb7049ac52c6d66
bhuvaneshcmd <bhuvaneshdvs@gmail.com>
2024-08-13 19:56:29 +0530

    created a vpc module

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "cidr" {
+    type =string
+}
+variable "DNS_Host_Names" {
+    type =bool
+}
+variable "DNS_Support" {
+    type = bool
+}
+variable "tag_value" {
+    type = string
+}
---
+++ vpc.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr
+  enable_dns_hostnames = var.DNS_Host_Names
+  enable_dns_support   = var.DNS_Support
+  tags = {
+    Name = var.tag_value
+  }
+}
