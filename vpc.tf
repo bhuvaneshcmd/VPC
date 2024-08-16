3fbdc1b4f03e9aa49fc17c10bff951bac4eb903b
bhuvaneshcmd <bhuvaneshdvs@gmail.com>
2024-08-16 12:05:33 +0530

    added route to the route table

--- vpc.tf
+++ vpc.tf
@@ -10,6 +10,14 @@ resource "aws_vpc" "this" {
   }
 }
 
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
 resource "aws_subnet" "public" {
   count             = length(var.public_subnet_cidrs)
   vpc_id            = aws_vpc.this.id
@@ -24,26 +32,21 @@ resource "aws_subnet" "public" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-
   tags = {
-    Name = "${var.name}-public"
+    Name = "${var.name}-public-rt"
   }
+}
+
+resource "aws_route" "internet_access" {
+  count = length(var.public_subnet_cidrs)
+
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
 }
 
 resource "aws_route_table_association" "public" {
-  count      = length(var.public_subnet_cidrs)
-  subnet_id  = aws_subnet.public[count.index].id
+  count        = length(var.public_subnet_cidrs)
+  subnet_id    = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
-}
-
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = "${var.name}-igw"
-  }
 }
