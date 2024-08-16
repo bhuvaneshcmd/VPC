035dad6a9c8b937dd8f851ab4bd97e5689882e84
bhuvaneshcmd <bhuvaneshdvs@gmail.com>
2024-08-16 11:31:28 +0530

    removed count.index in subnet_id at route table attach

--- vpc.tf
+++ vpc.tf
@@ -43,7 +43,7 @@ resource "aws_route" "public_internet_gateway" {
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public[count.index].id
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
 
