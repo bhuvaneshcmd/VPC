b09f4646758704b69de7297d5fabbf4a40e371ea
bhuvaneshcmd <bhuvaneshdvs@gmail.com>
2024-08-16 10:42:19 +0530

    updated VPC module with internet gateway

--- vpc.tf
+++ vpc.tf
@@ -21,3 +21,29 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "internetgateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "publicroutetable"
+  }
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ig.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.this.id
+  route_table_id = aws_route_table.public.id
+}
+
+
